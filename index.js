34c43f4788858955f7a1374a169fe2f34bb34dcd
pjames27 <44187608+pjames27@users.noreply.github.com>
2023-07-08 15:02:34 -0700

    Initial linking js and css
    - Separated css for index out into its own file
    - Changed id of main div from "m" to "level_body"
    - Now link separate js files for index and levels

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function update_level(level){
         
     $(function() {
-       $("#m").load("level_" + level+".html");
+       $("#level_body").load("level_" + level+".html");
     });
 
 }
 
 function fail_level(){
-    let level_body = document.getElementById("m");
+    let level_body = document.getElementById("level_body");
     level_body.style.pointerEvents = false;
 }
 
